04851216aed6813319e3890b1c95f8891d737b07
dahee90522 <ekgml0522@knou.ac.kr>
2023-04-07 19:54:06 +0900

    ♻️ Change folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import './index.css';
-
-import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Router from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
 );
